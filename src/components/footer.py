23e3ed9b1f02ca01dec357540f9fa632e5e08628
tauhid-topu <t.topu021@gmail.com>
2026-06-16 20:52:06 +0600

    student dashboard

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "/images/logoai.png"
+    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
@@ -14,11 +14,11 @@ def footer_home():
 
 
 def footer_dashboard():
-    logo_url = "/images/logoai.png"
+    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by</p>  
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
